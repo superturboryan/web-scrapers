cd4fe984d5b8082a204c6080206ce9a0ae63640a
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-26 10:55:03 -0400

    Write results to .csv file

--- CodeWarsTop500.js
+++ CodeWarsTop500.js
@@ -1,5 +1,13 @@
 const request = require("request")
 const cheerio = require("cheerio")
+const fs = require("fs")
+const writeStream = fs.createWriteStream("Top500CodeWars.csv")
+
+const timeStamp = new Date().toLocaleDateString
+
+//Write headers
+writeStream.write("Top 500 users of CodeWars by honor \n")
+writeStream.write("Date: " + timeStamp)
 
 //Scrape and list the top 500 users of CodeWars
 
@@ -9,13 +17,14 @@ request("https://www.codewars.com/users/leaderboard", (error, response, html) =>
 
       const $ = cheerio.load(html)
 
-      console.log("Top 500 CodeWars users by honor")
       $("tbody tr td.is-big a").each((i, el) => {
 
          const username = $(el).attr("href").substr(7)
 
-         console.log((i + 1) + ")", username)
+         //Write to csv
+         writeStream.write(`${i}) ${username}`)
       })
 
+      console.log("Scraping done")
    }
 })
